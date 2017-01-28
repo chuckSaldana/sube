03ed45b1ecde8f7e599613d992c7d729b35118d1
Carlos Saldana <csaldana@apocalapsus.luxoft.com>
2017-01-28 13:01:46 -0600

    Added functionality to basic info.

--- sube/sube/Controllers/BasicInfoViewController.swift
+++ sube/sube/Controllers/BasicInfoViewController.swift
@@ -8,29 +8,38 @@
 
 import UIKit
 
-class BasicInfoViewController: UIViewController/*, UIPickerViewDataSource, UIPickerViewDelegate */{
+class BasicInfoViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var cityTxtF: UITextField!
+    @IBOutlet weak var stateTxtF: UITextField!
     @IBOutlet weak var picker: UIPickerView!
-    var loadStates = false
+    var selectedState = 0
     var loadCities = false
+    let statesArray = ["CDMX", "Jalisco", "Nuevo Leon"]
+    let citiesArray = [["Milpa Alta", "Tláhuac", "Tlalpan"], ["Guadlajara", "Tlaquepaque", "Zapopan"], ["Apodaca", "Escobedo", "Guadalupe"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(animated: Bool) {
         picker?.hidden = true
     }
 
     @IBAction func selectState(sender: AnyObject) {
         self.picker?.hidden = false
-        self.loadStates = true
         self.loadCities = false
+        self.picker.reloadAllComponents()
     }
     
     @IBAction func selectCity(sender: AnyObject) {
         self.picker?.hidden = false
         self.loadCities = true
-        self.loadStates = false
+
+        self.selectedState = picker.selectedRowInComponent(0)
+        self.picker.reloadAllComponents()
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,9 +49,10 @@ class BasicInfoViewController: UIViewController/*, UIPickerViewDataSource, UIPic
     
 
     @IBAction func useCurrentLocation(sender: AnyObject) {
+
     }
-    @IBOutlet weak var `continue`: UIButton!
-    /*
+
+         /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -52,12 +62,25 @@ class BasicInfoViewController: UIViewController/*, UIPickerViewDataSource, UIPic
     }
     */
     
-   /* func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
-    }*/
-    
+        return self.loadCities ? self.citiesArray[selectedState].count : self.statesArray.count
+    }
+
+
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.loadCities ? self.citiesArray[selectedState][row] : self.statesArray[row]
+    }
+
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if self.loadCities {
+            self.cityTxtF.text = self.citiesArray[selectedState][row]
+        }
+        else {
+            self.stateTxtF.text = self.statesArray[row]
+        }
+    }
 }
